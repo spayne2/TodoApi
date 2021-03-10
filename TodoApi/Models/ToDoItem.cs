65e01abae5dcb0fa3ce7eea42bd8f454cda24be5
Simon Payne <simon.payne@southwales.ac.uk>
2021-03-10 10:58:38 +0000

    Add project files.

---
+++ TodoApi/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
